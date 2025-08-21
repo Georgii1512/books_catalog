66656b72d561c8b41f916f34f3ca0f4ee6ba15d6
Georgii1512 <153933316+Georgii1512@users.noreply.github.com>
2025-08-21 18:29:35 +0300

    Add help text to the fields, set the year field as null=True

--- catalog/models.py
+++ catalog/models.py
@@ -3,14 +3,40 @@ from django.db import models
 # Create your models here.
 NA = 'N/A'
 
+
 class CatalogBook(models.Model):
-    author = models.CharField(max_length=200, blank=True, default=NA)
-    title = models.CharField(max_length=200, unique=True)
-    seria = models.CharField(max_length=200, blank=True, default=NA)
-    pages_count = models.PositiveSmallIntegerField(blank=True, default=NA)
-    year = models.PositiveSmallIntegerField(blank=True, default=0)
-    language = models.CharField(max_length=200, blank=True, default=NA)
-    isbn = models.CharField(max_length=13, unique=True, primary_key=True)
+    author = models.CharField(
+        max_length=200,
+        blank=True,
+        default=NA,
+        help_text='Enter author name')
+    title = models.CharField(
+        max_length=200,
+        unique=True,
+        help_text='Enter book title')
+    seria = models.CharField(
+        max_length=200,
+        blank=True,
+        default=NA,
+        help_text='Enter book seria')
+    pages_count = models.PositiveSmallIntegerField(
+        blank=True,
+        default=NA,
+        help_text='Enter book pages count')
+    year = models.PositiveSmallIntegerField(
+        blank=True,
+        null=True,
+        help_text='Enter book year')
+    language = models.CharField(
+        max_length=200,
+        blank=True,
+        default=NA,
+        help_text='Enter number of the pages in book')
+    isbn = models.CharField(
+        max_length=13,
+        unique=True,
+        primary_key=True,
+        help_text='Enter book ISBN')
 
     class Meta:
         ordering = ['title']
