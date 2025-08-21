aca9ac5dd5ce0579766726b3ca938cca9771fb2f
Georgii1512 <153933316+Georgii1512@users.noreply.github.com>
2025-08-21 18:17:34 +0300

    Add a catalog books views and pages

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,4 +5,9 @@ app_name = 'catalog'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('catalog/create/', views.CatalogBookCreateView.as_view(), name='book_create'),
+    path('catalog/books-list/', views.CatalogBooksListView.as_view(), name='books_list'),
+    path('catalog/book/<str:pk>/update/', views.CatalogBookUpdateView.as_view(), name='book_update'),
+    path('catalog/book/<str:pk>/delete/', views.CatalogBookDeleteView.as_view(), name='book_delete'),
+
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,36 @@
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, TemplateView, DeleteView, UpdateView
+from .forms import CatalogBookForm
+from .models import CatalogBook
 
 # Create your views here.
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+
+class CatalogBooksListView(ListView):
+    template_name = 'books-list.html'
+    queryset = CatalogBook.objects.all()
+    context_object_name = 'catalog_books'
+
+
+class CatalogBookCreateView(CreateView):
+    template_name = 'catalog-book-create.html'
+    form_class = CatalogBookForm
+    success_url = reverse_lazy('catalog:books_list')
+    context_object_name = 'catalog_book'
+
+class CatalogBookDeleteView(DeleteView):
+    template_name = 'catalog-book-delete.html'
+    model = CatalogBook
+    success_url = reverse_lazy('catalog:books_list')
+    context_object_name = 'catalog_book'
+
+
+class CatalogBookUpdateView(UpdateView):
+    template_name = 'catalog-book-update.html'
+    model = CatalogBook
+    form_class = CatalogBookForm
+    context_object_name = 'catalog_book'
+    success_url = reverse_lazy('catalog:books_list')
